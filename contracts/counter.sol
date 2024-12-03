091824d84f0d2047134471c42e2ce6bb047388ce
zhouming <minerchow@126.com>
2024-12-03 23:36:29 +0800

    update

---
+++ contracts/counter.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+contract Counter {
+    uint counter;
+
+    constructor() {
+        counter = 0;
+    }
+
+    function count() public {
+        counter = counter + 1;
+    }
+
+    function get() public view returns (uint) {
+        return counter;
+    }
+}
